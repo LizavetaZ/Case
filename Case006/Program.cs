e2040cff9a2101d1ace7bb2c8f445833c1a51172
Лиза за <lizuska@yandex.ru>
2022-10-02 00:00:28 +0300

    find an even number

---
+++ Case006/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+string value = Console.ReadLine();
+int a = int.Parse(value);
+if (a % 2 == 0) 
+Console.WriteLine("четное");
+else Console.WriteLine("нечетное");
+
